705422c8ea00bb9d1eb98417986e1f6b5da30f89
Alejandro Palacio <alejandrooss94@gmail.com>
2020-10-05 16:14:03 -0500

    Estructura y templates de la SPA listos

---
+++ src/page/Home.js
@@ -0,0 +1,15 @@
+const Home = () => {
+    const view = `
+        <div class="character">
+            <article class="character-item">
+                <a href='#/1/'>
+                    <img src="image" alt="name">
+                    <h2>Name</h2>
+                </a>
+            </article>
+        </div>
+    `//comillas fransesas
+    return view; 
+}
+
+export default Home;
